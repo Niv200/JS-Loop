7d7b3e8c9ccb7b71df3735d6172318a858193f88
Nivm200 <nivm200@gmail.com>
2021-01-18 13:14:44 +0200

    Finished Q4

---
+++ Q4.js
@@ -0,0 +1,11 @@
+alert("Question 4");
+let firstNumber = Number(prompt("First number"));
+let secondNumber = Number(prompt("Second number"));
+let thirdNumber = Number(prompt("Third number"));
+let fourthNumber = Number(prompt("Fourth number"));
+let fifthNumber = Number(prompt("Fifth number"));
+let largest = Math.max(firstNumber, secondNumber, thirdNumber, fourthNumber, fifthNumber);
+
+
+
+alert("The largest number is " + largest);
