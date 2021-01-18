e9c8cfad24a16a59d142402938d40ea5e6bf5203
Nivm200 <nivm200@gmail.com>
2021-01-18 13:20:09 +0200

    Finished Q5
    Added file for Q6

---
+++ Q5.js
@@ -0,0 +1,16 @@
+alert("Question 5");
+let i = 0;
+for(i; i <= 15; i++){
+    if(i === 0){
+        alert("0 is even");
+    }else{
+        if(i % 2 === 0){
+            alert(i + " is even");
+        }else{
+            alert(i + " is odd");
+        }
+    }
+}
+
+
+alert("The largest number is " + largest);
