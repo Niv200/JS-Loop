ed8071209e300ca0bec67ddbebe0d8e802308175
Nivm200 <nivm200@gmail.com>
2021-01-18 13:48:56 +0200

    Finished Q6
    Finished Q6 working on 8 or maybe skipping

--- Q6.js
+++ Q6.js
@@ -1,1 +1,21 @@
-let David = ;
+let students = [["David", 80], ['Vinoth', 77], ['Divya', 88], ['Ishitha', 95], ['Thomas', 68]];
+
+let avg = 0;
+let total = 0;
+
+for(let i = 0; i < students.length; i++){
+    total = total + students[i][1];
+}
+avg = total / students.length;
+alert("Average: " + avg);
+if(avg < 60){
+    alert("Average grade: F")
+}else if(avg < 70){
+    alert("Average grade: D")
+}else if(avg < 80){
+    alert("Average grade: C")
+}else if(avg < 90){
+    alert("Average grade: B")
+}else if(avg < 100){
+    alert("Average grade: A")
+}
---
+++ Q8.js
@@ -0,0 +1,8 @@
+let count = 1;
+let number = 1;
+while(count <= 5){
+    let numbers = number.toString();
+    for(i = 0; i <= numbers.length-1; i++){
+        alert(i);
+    }
+}
