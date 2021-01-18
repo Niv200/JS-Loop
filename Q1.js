1ac4a339f3c8d71cb1e6881e1b37930ef253921b
Nivm200 <nivm200@gmail.com>
2021-01-18 11:59:10 +0200

    Javascript Q1

---
+++ Q1.js
@@ -0,0 +1,14 @@
+alert("hello");
+let firstNumber = prompt("First number");
+let secondNumber = prompt("Second number");
+
+if(firstNumber > secondNumber){
+    alert("First is bigger than second");
+}
+if(secondNumber > firstNumber){
+    alert("Second is bigger than first");
+}
+if(firstNumber === secondNumber){
+    alert("First and second numbers are equal");
+}
+
